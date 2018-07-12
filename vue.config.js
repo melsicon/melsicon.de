538f30550d1a1017a8b1e6b334574718222499c5
vicbergquist <victoria.bergquist@gmail.com>
2018-07-12 14:56:59 +0200

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    css: {
-        sourceMap: true,
-        loaderOptions: {
-            // pass options to sass-loader
-            sass: {
-                data: `@import "@/variables.sass";`
-            }
-        }
-    },
-    lintOnSave: true,
-    pluginOptions: {
-        enableInSFC: false
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        data: `@import "@/variables.sass";`
+      }
     }
+  },
+  lintOnSave: true,
+  pluginOptions: {
+    enableInSFC: false
+  }
 }

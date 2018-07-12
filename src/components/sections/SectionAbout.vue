c8cb55b2368b377705c5a4bfe22cce2bfabd8a00
vicbergquist <victoria.bergquist@gmail.com>
2018-07-12 15:08:28 +0200

    added services (mock)

--- src/components/sections/SectionAbout.vue
+++ src/components/sections/SectionAbout.vue
@@ -5,6 +5,16 @@
             <h2 class="sub-heading">{{melsicon.title}}</h2>
             <p v-for="paragraph in melsicon.paragraphs" :key="paragraph.index" class="span2">{{paragraph}}</p>
         </section>
+        <section class="services">
+            <h3 class="sub-heading">{{services.title}}</h3>
+            <p v-for="paragraph in services.paragraphs" :key="paragraph.index">{{paragraph}}</p>
+            <ul class="service-list">
+                <li v-for="service in services.list" :key="service.index" class="service">
+                    <h4>{{service.name}}</h4>
+                    <p>{{service.description}}</p>
+                </li>
+            </ul>
+        </section>
     </article>
 </template>
 
@@ -17,6 +27,26 @@ export default {
       paragraphs: [
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         'Our team of developers sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      ]
+    },
+    services: {
+      title: 'Our services',
+      paragraphs: [
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      ],
+      list: [
+        {
+          name: 'Service 1',
+          description: 'Service 1 description'
+        },
+        {
+          name: 'Service 2',
+          description: 'Service 2 description'
+        },
+        {
+          name: 'Service 3',
+          description: 'Service 3 description'
+        }
       ]
     }
   })

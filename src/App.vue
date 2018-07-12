90c5bb0f20697695ccc864662e24911f84beb8df
vicbergquist <victoria.bergquist@gmail.com>
2018-07-12 15:00:14 +0200

    global styles

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@ import TheNav from '@/components/TheNav'
 import TheFooter from '@/components/TheFooter'
 
 export default {
-    components: {
-        TheNav,
-        TheFooter
-    }
+  components: {
+    TheNav,
+    TheFooter
+  }
 }
 </script>
 
@@ -29,6 +29,8 @@ export default {
     
   *
     box-sizing: border-box
+    margin: 0
+    padding: 0
 
   h1, h2, h3
     font-family: $font-heading
@@ -37,44 +39,47 @@ export default {
   ul
     list-style-type: none
 
-  p:not(:last-child)
-    margin-bottom: 1rem
-
   * + *
     margin: 0
     padding: 0
 
   /* GENERAL ELEMENTS */
 
-  .illustration 
-    align-self: flex-start
-    margin: 1.5rem auto
+  .illustration
+    background: $color-primary-lighter
+    height: 300px
+    margin: 1rem 0
 
-    div
-      height: 100px
-      width: 100px
-      background: $color-primary-lighter
-
-  section
+  .article-section
     display: grid
-    grid-template-columns: repeat(2, 1fr) 
+    grid-template-columns: repeat(2, 1fr)
+    grid-auto-rows: max-content
     flex-wrap: wrap
-    grid-gap: 1rem
+    grid-gap: .5rem 1rem
     grid-column: 1 / -1
-    margin-top: 3rem
+    margin: 3rem 0
 
-  h1
-    grid-column: 1 / -1
-    font-size: 3.5rem
-    margin-top: 2rem
-    margin-bottom: 1rem
+    section
+      display: grid
+      grid-template-columns: repeat(2, 1fr)
+      grid-auto-rows: max-content
+
+  p
+    margin: 0
+    grid-column: span 1
+    &:not(:last-child)
+      margin-bottom: 1rem
+    &.span2
+      grid-column: span 2
 
   h2
+    grid-column: 1 / -1
     margin-bottom: 1rem
-    font-size: 2.5rem
+    font-size: 2.2rem
 
   h3
+    grid-column: 1 / -1
     margin-top: 1rem
     margin-bottom: .5rem
-    font-size: 1.5rem
+    font-size: 1.8rem
 </style>

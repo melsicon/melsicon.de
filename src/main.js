9c47d73af734415cf7ce593a044ace1b7fb8e871
vicbergquist <victoria.bergquist@gmail.com>
2018-07-19 11:02:50 +0200

    installed media query and scroll packages

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import i18n from './i18n'
+import VueScrollactive from 'vue-scrollactive'
+import VueMq from 'vue-mq'
 
 Vue.config.productionTip = false
+
+Vue.use(VueScrollactive)
+
+Vue.use(VueMq, {
+  breakpoints: {
+    sm: 800,
+    md: 1250,
+    lg: Infinity
+  }
+})
 
 new Vue({
   router,

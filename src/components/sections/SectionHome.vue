9a8b5bb2bd35a1e8348dca19d5cc8736679f42cd
vicbergquist <victoria.bergquist@gmail.com>
2018-07-19 19:09:26 +0200

    added l18n to home section

--- src/components/sections/SectionHome.vue
+++ src/components/sections/SectionHome.vue
@@ -1,8 +1,8 @@
 <template>
-    <section class="section intro">
+    <section id="home" class="section home">
         <article class="article">
-            <h1 class="main-heading">{{title}}</h1>
-            <p>{{description}}</p>
+            <h1 class="main-heading">{{ $t('home.heading') }}</h1>
+            <p v-for="p in $t('home.paragraphs')" :key="p.index">{{p}}</p>
             <span class="span2 btn-wrap"><button>Contact us</button></span>
         </article>
     </section>
@@ -10,11 +10,6 @@
 
 <script>
 export default {
-  name: 'Home',
-  data: () => ({
-    title: 'Draw the audience in with a fancy heading, like a baws',
-    description:
-      'Lorem ipsum dolor sit ame, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-  })
+  name: 'Home'
 }
 </script>

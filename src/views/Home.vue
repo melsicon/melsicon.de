132f0627c0a817d108f27f7d9ea1565d72002cbf
vicbergquist <victoria.bergquist@gmail.com>
2018-07-19 19:08:44 +0200

    added some responsiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,13 +34,24 @@ export default {
   max-width: 1200px
   min-height: 100vh
   margin: auto
-  padding: 0 .5rem
+  @media only screen and (min-width: 1480px)
+    max-width: 1400px
 
 .section
-  margin-top: 2rem
-  margin-bottom: 3rem
+  min-height: 70vh
+  display: flex
+  justify-content: center
+  flex-direction: column
   @media screen and (max-width: 768px)
     margin: 1rem 0
+
+.home
+  min-height: 80vh
+  display: flex
+  justify-content: center
+
+  .article
+    margin-bottom: 5rem
 
 .article
   display: grid
@@ -65,11 +76,5 @@ export default {
   @media screen and (max-width: 768px)
     margin: 1.5rem 0
     flex-direction: column
-
-  li
-    color: $color-secondary
-    background: $color-primary-lighter
-
-
 
 </style>

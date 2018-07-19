0190b4c7729fba7bf7afe1b8de8638be652f083d
vicbergquist <victoria.bergquist@gmail.com>
2018-07-19 19:27:31 +0200

    put content in a wrapper div for layout reasons

--- src/components/sections/SectionContact.vue
+++ src/components/sections/SectionContact.vue
@@ -2,8 +2,10 @@
     <section id="contact" class="section contact">
         <article class="article">
             <h2 class="sub-heading span2">{{ $t('contact.heading') }}</h2>
-            <p v-for="p in $t('contact.paragraphs')" :key="p.index">{{p}}</p>
-            <span class="btn-wrap"><button>{{ $t('buttons.contact') }}</button></span>
+            <div>
+                <p v-for="p in $t('contact.paragraphs')" :key="p.index">{{p}}</p>
+                <span class="btn-wrap"><button>{{ $t('buttons.contact') }}</button></span>
+            </div>
             <div class="illustration">
                 <temp-illustration/>
             </div>

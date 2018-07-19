cdef37ee18c75c320ea7c6c7deceb63ae7e8183f
vicbergquist <victoria.bergquist@gmail.com>
2018-07-19 11:04:24 +0200

    updated router to include profile page route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Profiles from './views/Profiles'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/profiles',
+      name: 'profiles',
+      component: Profiles
     }
   ]
 })

fe5a458759fdc789a7caf61db608d240b8a09113
vicbergquist <victoria.bergquist@gmail.com>
2018-07-20 16:36:23 +0200

    added mobile sidebar

---
+++ src/components/SidebarToggle.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="nav-toggle">
+        <button class="btn btn-open" @click="$emit('toggleSidebar')">{{ $t('buttons.open') }}</button>
+        <the-logo/>
+    </nav>
+</template>
+
+<script>
+import TheLogo from '@/components/graphics/TheLogo'
+export default {
+  components: {
+    TheLogo
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+  .nav-toggle
+    position: sticky
+    top: 0
+    width: 100%
+    display: flex
+    justify-content: space-between
+    background: $color-white
+    min-height: 2rem
+    z-index: 2
+    opacity: 1
+    padding: .5rem .8rem
+    transition: opacity .5s ease-in-out
+  
+  .hide
+    opacity: 0
+
+  .btn-open
+    color: $color-body
+  
+  .logo
+    fill: $color-primary
+    max-height: 1.8rem
+</style>
---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,79 @@
+<template>
+    <aside class="sidebar">
+        <div class="top">
+            <button class="btn btn-close" @click="$emit('toggleSidebar')">{{ $t('buttons.close') }}</button>
+            <the-logo/>
+        </div>
+        <scrollactive :offset="100" 
+                      :modify-url="false"
+                      class="sidebar-nav">
+            <a v-for="(route, key) in $t('nav')" 
+               :key="route.index" 
+               :href="'#' + key"
+               class="nav-link sb-nav-link scrollactive-item"
+               @click="$emit('toggleSidebar')"
+            >{{route}}</a>
+        </scrollactive>
+        <ul class="socialmedia-list">
+            <li class="socialmedia-item">Twitter</li>
+            <li class="socialmedia-item">LinkedIn</li>
+            <li class="socialmedia-item">XING</li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+import TheLogo from '@/components/graphics/TheLogo'
+export default {
+  components: {
+    TheLogo
+  }
+}
+</script>
+
+
+<style lang="sass">
+  .sidebar
+    position: fixed
+    top: 0
+    left: 0
+    overflow: hidden
+    display: flex
+    flex-direction: column
+    align-content: space-between
+    height: 100vh
+    width: 100%
+    z-index: 100
+    background: $color-secondary-darker
+    padding: .5rem .8rem
+
+  .top
+    display: flex
+    justify-content: space-between
+
+  .logo
+    max-height: 1.8rem
+    fill: $color-white
+
+  .btn-close
+    color: $color-white
+
+  .sidebar-nav
+    align-self: center
+    display: flex
+    flex-direction: column
+    align-items: flex-start
+    margin: auto
+
+  .sb-nav-link
+    font-size: 1.5rem
+    color: $color-white
+    margin-bottom: 1rem
+  
+  .socialmedia-list
+    display: flex
+    justify-content: space-between
+    color: white
+    margin: 2rem
+
+</style>

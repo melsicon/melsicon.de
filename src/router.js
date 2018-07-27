9ec6de230596f5c9904cf25e8428d00debb7b8ac
vicbergquist <victoria.bergquist@gmail.com>
2018-07-27 15:10:24 +0200

    changed page name to team

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Meta from 'vue-meta'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
 import Team from './views/Team'
 
 Vue.use(Router)
 Vue.use(Meta)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -15,9 +16,24 @@ export default new Router({
       component: Home
     },
     {
-      path: '/profiles',
-      name: 'profiles',
-      component: Team
+      path: '/team',
+      name: 'team',
+      component: Team,
+      meta: {
+        scrollToTop: true
+      }
     }
-  ]
+  ],
+  scrollBehavior: (to, from, savedPosition) => {
+    if (to.hash) {
+      return {
+        selector: to.hash
+      }
+    }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {x: 0, y: 0}
+    }
+  }
 })

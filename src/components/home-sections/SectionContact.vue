12c4b90875ddfbd0ff331d48d1a9953a115d10f2
vicbergquist <victoria.bergquist@gmail.com>
2018-07-27 15:13:06 +0200

    removed button wrap

--- src/components/home-sections/SectionContact.vue
+++ src/components/home-sections/SectionContact.vue
@@ -3,7 +3,7 @@
         <article class="article">
             <h2 class="sub-heading span2">{{ $t('contact.heading') }}</h2>
             <p v-for="p in $t('contact.paragraphs')" :key="p.index">{{p}}</p>
-            <span class="btn-wrap"><button class="btn btn-cta">{{ $t('buttons.contact') }}</button></span>
+            <button class="btn btn-cta">{{ $t('buttons.contact') }}</button>
         </article>
         <div class="illustration"/>
     </section>

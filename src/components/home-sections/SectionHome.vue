b538b957e1a9af05d1dbf8081127c712348c4ce7
vicbergquist <victoria.bergquist@gmail.com>
2018-08-01 09:19:11 +0200

    added illustration and more link

--- src/components/home-sections/SectionHome.vue
+++ src/components/home-sections/SectionHome.vue
@@ -3,7 +3,8 @@
         <article class="article d-contents">
             <h1 class="main-heading span-full">{{ $t('home.heading') }}</h1>
             <p v-for="p in $t('home.paragraphs')" :key="p.index">{{p}}</p>
-            <span class="span2 btn-wrap"><button class="btn btn-more">Learn more</button></span>
+            <a href="#services" class="link-more">{{ $t('buttons.more') }}</a>
+            <img src="" alt="" class="illustration illustration-home">
         </article>
     </section>
 </template>
@@ -13,3 +14,13 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="sass" scoped>
+
+  .home
+    padding: 5em 0
+
+  .link-more
+    grid-column: 1
+
+</style>

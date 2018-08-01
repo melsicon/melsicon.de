a1636bd762e1b53b798d0c21db0b700c735c7591
vicbergquist <victoria.bergquist@gmail.com>
2018-08-01 09:18:34 +0200

    reordered content

--- src/components/home-sections/SectionContact.vue
+++ src/components/home-sections/SectionContact.vue
@@ -1,10 +1,10 @@
 <template>
     <section id="contact" class="section contact">
+        <img src="" alt="" class="illustration illustration-contact">
         <article class="article">
             <h2 class="sub-heading span2">{{ $t('contact.heading') }}</h2>
             <p v-for="p in $t('contact.paragraphs')" :key="p.index">{{p}}</p>
             <button class="btn btn-cta">{{ $t('buttons.contact') }}</button>
         </article>
-        <div class="illustration"/>
     </section>
 </template>

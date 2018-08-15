5b5ef14810160cdf96564748af5610926e5316e7
vicbergquist <victoria.bergquist@gmail.com>
2018-08-15 11:03:49 +0200

    added team page component intro

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,14 +1,17 @@
 <template>
     <main class="wrapper">
+        <section-intro/>
         <section-profile/>
     </main>
 </template>
 
 <script>
 import SectionProfile from '@/components/team-sections/SectionProfile'
+import SectionIntro from '@/components/team-sections/SectionIntro'
 export default {
   components: {
-    SectionProfile
+    SectionProfile,
+    SectionIntro
   }
 }
 </script>

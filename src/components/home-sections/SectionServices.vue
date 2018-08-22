8425955495b558050e0e6ad12f63834da4e85c1a
vicbergquist <victoria.bergquist@gmail.com>
2018-08-22 20:58:15 +0200

    added client

--- src/components/home-sections/SectionServices.vue
+++ src/components/home-sections/SectionServices.vue
@@ -3,13 +3,31 @@
         id="services"
         class="section section-services">
         <illustration-services/>
-        <article class="article">
+        <article class="article article-intro">
             <h2 class="heading">{{ $t('services.heading') }}</h2>
             <p
                 v-for="p in $t('services.paragraphs')"
                 :key="p.index"
                 class="paragraph">{{ p }}</p>
         </article>
+        <article class="article article-industry">
+            <h3 class="sub-heading">{{ $t('services.bank.heading') }}</h3>
+            <p
+                v-for="p in $t('services.bank.paragraphs')"
+                :key="p.index"
+                class="paragraph">{{ p }}</p>
+            <h3 class="sub-heading">{{ $t('services.transport.heading') }}</h3>
+            <p
+                v-for="p in $t('services.transport.paragraphs')"
+                :key="p.index"
+                class="paragraph">{{ p }}</p>
+        </article>
+        <ul class="list client-list">
+            <li class="client"><the-deutschebank/></li>
+            <li class="client"><the-commerzbank/></li>
+            <li class="client"><the-amadeus/></li>
+            <li class="client"><the-db/></li>
+        </ul>
     </section>
 </template>
 
@@ -17,15 +35,15 @@
 import TheCommerzbank from '@/components/graphics/clients/TheCommerzbank'
 import TheDeutschebank from '@/components/graphics/clients/TheDeutschebank'
 import TheDb from '@/components/graphics/clients/TheDb'
+import TheAmadeus from '@/components/graphics/clients/TheAmadeus'
 import IllustrationServices from '@/components/graphics/illustrations/IllustrationServices'
-import IllustrationIndustry from '@/components/graphics/illustrations/IllustrationIndustry'
 export default {
   components: {
     TheCommerzbank,
     TheDeutschebank,
     TheDb,
-    IllustrationServices,
-    IllustrationIndustry
+    TheAmadeus,
+    IllustrationServices
   }
 }
 </script>
@@ -33,7 +51,7 @@ export default {
 <style lang="sass">
 
   .section-services
-    grid-row-gap: 1.5em
+    align-items: center
 
   .client-list
     display: flex
@@ -44,7 +62,7 @@ export default {
     .client
       margin: .5em 0
 
-  .commerzbank, .deutschebank, .db
+  .commerzbank, .deutschebank, .db, .amadeus
     fill: $color-grey
     margin: .5em 0
 
@@ -57,7 +75,19 @@ export default {
   .db
     height: 4em
 
+  .amadeus
+    height: 2em
+
   @media screen and (max-width: 1000px)
+    .article-intro
+      order: 1
+
+    .article-industry
+      order: 3
+
+    .client-list
+      order: 4
+
     .client
       margin: .5em 0
 

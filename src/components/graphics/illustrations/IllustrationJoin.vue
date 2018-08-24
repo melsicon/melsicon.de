d8408d576d13864e530623aad609f5958fec893b
vicbergquist <victoria.bergquist@gmail.com>
2018-08-24 14:44:00 +0200

    renamed

--- src/components/graphics/illustrations/IllustrationJoin.vue
+++ src/components/graphics/illustrations/IllustrationJoin.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="illustration illustration-jobs">
+    <div class="illustration illustration-join">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 500 462">

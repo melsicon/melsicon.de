1a9e560458505aefde6e1ca2c0e797e54a9c5983
vicbergquist <victoria.bergquist@gmail.com>
2018-08-24 14:43:43 +0200

    routing

--- src/router.js
+++ src/router.js
@@ -3,8 +3,6 @@ import Meta from 'vue-meta'
 import Router from 'vue-router'
 // Pages
 import Home from '@/views/Home'
-const Team = () => import('@/views/Team')
-const Jobs = () => import('@/views/Jobs')
 const NotFound = () => import('@/views/NotFound')
 
 Vue.use(Router)
@@ -19,31 +17,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/team',
-      name: 'team',
-      component: Team
-    },
-    {
-      path: '/jobs',
-      name: 'jobs',
-      component: Jobs
-    },
-    {
       path: '*',
       name: '404',
       component: NotFound
     }
-  ],
-  scrollBehavior: (to, from, savedPosition) => {
-    if (to.hash) {
-      return {
-        selector: to.hash
-      }
-    }
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return {x: 0, y: 0}
-    }
-  }
+  ]
 })

bbcd5b90fe1f9f283f6091847acb261fb7c88d18
vicbergquist <victoria.bergquist@gmail.com>
2019-01-23 12:08:58 +0100

    updated error page to make sure consent is tracked properly

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="section section-error">
+    <!-- TODO: Insert 404 illustration -->
     <h2 class="error-heading">{{ $t('error.heading') }}</h2>
     <p>{{ $t('error.desc') }}
       <i18n
@@ -27,3 +28,29 @@ export default {
   layout: 'ErrorLayout'
 }
 </script>
+
+<style lang="sass" scoped>
+
+.section-error
+  display: flex
+  min-height: 100vh
+  text-align: center
+  margin: auto;
+  flex-direction: column
+  padding-bottom: 3em
+  padding: 5em 0
+
+.error-heading
+  color: $color-black
+  font-size: 4em
+  font-weight: 200
+  margin: 0
+  margin-bottom: 0
+
+.error-link
+  color: $color-primary-darker
+
+.paragraph-error
+  color: $color-black
+
+</style>
